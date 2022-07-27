08ff8975ccdaaf1d42ed66b36cda63356fa8d81d
melvincabatuan <melvincabatuan@gmail.com>
2022-07-27 14:03:23 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,152 @@
+/*
+ * File: Main.java
+ * ---------------------
+ * This class is a Matrix demo class
+ * Author: Cobalt - mkc
+ * Date modified: July 8, 2019
+ */
+
+import acm.program.*;
+
+public class Main extends ConsoleProgram {
+
+    public void run() {
+        Matrix matrixA = null;
+        Matrix matrixB = null;
+        Matrix matrixC = null;
+        int choice = 0;
+        do {
+            printMenu();
+            choice = readInt(">> ");
+            switch (choice) {
+
+                case 0: // Input Matrix
+                    println("matrixA: ");
+                    matrixA = inputHelper();  // enclose in try-catch block (optional)
+                    println("matrixB: ");
+                    matrixB = inputHelper();
+                    println("matrixA = ");
+                    println(matrixA.display());
+                    println("matrixB = ");
+                    println(matrixB.display());
+                    break;
+
+                case 1: // Addition
+                    if (matrixA == null || matrixB == null) {
+                        println("Please input matrices first.");
+                        break;
+                    }
+                    println("The sum of ");
+                    println("matrixA = ");
+                    println(matrixA.display());
+                    println("and matrixB = ");
+                    println(matrixB.display());
+                    println(" is equal to ");
+                    try {
+                        matrixC = matrixA.add(matrixB);
+                    } catch (Exception e) {
+                        println(" --- INVALID OPERATION --- ");
+                        e.printStackTrace();
+                        break;
+                    }
+                    println(matrixC.display());
+                    break;
+
+                case 2: // Subtraction
+                    if (matrixA == null || matrixB == null) {
+                        println("Please input matrices first.");
+                        break;
+                    }
+                    println("The difference of ");
+                    println("matrixA = ");
+                    println(matrixA.display());
+                    println("and matrixB = ");
+                    println(matrixB.display());
+                    println(" is equal to ");
+                    try {
+                        matrixC = matrixA.subtract(matrixB);
+                    } catch (Exception e) {
+                        println(" --- INVALID OPERATION --- ");
+                        e.printStackTrace();
+                        break;
+                    }
+                    println(matrixC.display());
+                    break;
+
+                case 3: // Transpose
+                    println("Input a matrix you want to transpose: ");
+                    println("matrixD: ");
+                    Matrix matrixD = inputHelper();
+                    println("matrixD = ");
+                    println(matrixD.display());
+                    println("The transpose is: ");
+                    matrixC = matrixD.transpose();
+                    println(matrixC.display());
+                    break;
+
+                case 4: // Multiplication
+                    if (matrixA == null || matrixB == null) {
+                        println("Please input matrices first.");
+                        break;
+                    }
+                    println("The product of ");
+                    println("matrixA = ");
+                    println(matrixA.display());
+                    println("and matrixB = ");
+                    println(matrixB.display());
+                    println(" is equal to ");
+                    try {
+                        matrixC = matrixA.multiply(matrixB);
+                    } catch (Exception e) {
+                        println(" --- INVALID OPERATION --- ");
+                        e.printStackTrace();
+                        break;
+                    }
+                    println(matrixC.display());
+                    break;
+
+                case 5:
+                    println("Exiting ....");
+                    pause(1000);
+                    exit();
+                    break;
+                default:
+                    println("Invalid");
+            }
+        } while (choice != 5);
+    }
+
+    public void printMenu(){
+        println("\n================================");
+        println("           Main Menu    ");
+        println("================================");
+        println(" 0. Input Matrix   ");
+        println(" 1. Addition   ");
+        println(" 2. Subtraction ");
+        println(" 3. Transpose ");
+        println(" 4. Multiplication ");
+        println(" 5. Exit ");
+        println("================================\n");
+    }
+
+    private Matrix inputHelper() {
+        println("Input the number of rows: ");
+        int rows = readInt(">> ");
+        println("Input the number of cols: ");
+        int cols = readInt(">> ");
+        if (rows == 0 || cols == 0)
+            throw new RuntimeException("Illegal matrix dimensions.");
+        double[][] result = new double[rows][cols];
+        println("Input the elements: ");
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = readDouble("matrix[" + i + "][" + j + "] >> ");
+            }
+        return new Matrix(result);
+    }
+
+    /* Solves NoClassDefFoundError */
+    public static void main(String[] args) {
+        new Main().start(args);
+    }
+}
---
+++ src/Matrix.java
@@ -0,0 +1,122 @@
+/**
+ * Matrix.java
+ * Author: cobalt - mkc
+ * Date created: July 8, 2019
+ * Date modified: July 27, 2022
+ */
+
+public class Matrix {
+    private int row; // number of matrix rows
+    private int col; // number of matrix columns
+    private double[][] data; // stores the elements
+
+    /**
+     * Constructor 1 : Creates the row x col matrix
+     * @param row  // number of matrix rows
+     * @param col  // number of matrix columns
+     */
+    public Matrix(int row, int col) {
+        this.row = row;
+        this.col = col;
+        data = new double[row][col];
+    }
+
+    /**
+     * Constructor 2 : Creates the row x col matrix from 2D array
+     * @param data
+     */
+    public Matrix(double[][] data) {
+        row = data.length;
+        col = data[0].length;
+        this.data = new double[row][col];
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++) {
+                this.data[i][j] = data[i][j];
+            }
+    }
+
+    /**
+     * Returns the number of rows
+     * @return
+     */
+    public int getRow() {
+        return row;
+    }
+
+    /**
+     * Returns the number of columns
+     * @return
+     */
+    public int getCol() {
+        return col;
+    }
+
+
+    /**
+     * Returns the transpose of the Matrix
+     * @return matrix transpose
+     */
+    Matrix transpose() {
+        if (row <= 0 || col <= 0)
+            throw new RuntimeException("Illegal matrix dimensions.");
+        Matrix result = new Matrix(col, row);
+        // ********************************************
+        // YOUR CODE HERE
+        // ********************************************
+        return result;
+    }
+
+    /**
+     * Returns the sum of the Matrix
+     * @param addend2
+     * @return
+     */
+    Matrix add(Matrix addend2) {
+        Matrix addend1 = this;
+        if (addend1.row != addend2.row || addend1.col != addend2.col)
+            throw new RuntimeException("Illegal matrix dimensions.");
+        Matrix sum = new Matrix(row, col);
+        // ********************************************
+        // YOUR CODE HERE
+        // ********************************************
+        return sum;
+    }
+
+    Matrix subtract(Matrix subtrahend) {
+        Matrix minuend = this;
+        if (subtrahend.row != minuend.row || subtrahend.col != minuend.col)
+            throw new RuntimeException("Illegal matrix dimensions.");
+        Matrix difference = new Matrix(row, col);
+        // ********************************************
+        // YOUR CODE HERE
+        // ********************************************
+        return difference;
+    }
+
+    Matrix multiply(Matrix factor2) {
+        Matrix factor1 = this;
+        if (factor1.col != factor2.row) throw new RuntimeException("Illegal matrix dimensions.");
+        Matrix product = new Matrix(factor1.row, factor2.col);
+        // ********************************************
+        // YOUR CODE HERE
+        // ********************************************
+        // ********************************************
+        return product;
+    }
+
+    /**
+     * This method displays the current matrix
+     * @return
+     */
+    String display() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                result.append(data[i][j]);
+                result.append("\t");
+            }
+            result.append("\n");
+        }
+        return result.toString();
+    }
+}
